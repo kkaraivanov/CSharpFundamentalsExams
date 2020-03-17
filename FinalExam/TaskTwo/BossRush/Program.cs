45b028c4bfbfea8723cdf6488ea2657da46fe5a4
Kostadin Karaivanov <kostadin.karaivanov@outlook.com>
2020-03-17 12:33:40 +0200

    update

---
+++ FinalExam/TaskTwo/BossRush/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace BossRush
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                string input = Console.ReadLine();
+                var name = Regex.Match(input, @"[|]([A-Z]+)[|]").Groups[1].ToString();
+                var title = Regex.Match(input, @"[#]([A-Za-z]+\s[A-Za-z]+)[#]").Groups[1].ToString();
+
+                if (InputIsValid(input))
+                {
+                    Console.WriteLine($"{name}, The {title}");
+                    Console.WriteLine($">> Strength: {name.Length}");
+                    Console.WriteLine($">> Armour: {title.Length}");
+                }
+                else
+                    Console.WriteLine("Access denied!");
+            }
+        }
+
+        static bool InputIsValid(string str)
+        {
+            return Regex.IsMatch(str, @"[|]([A-Z]+)[|]\:[#]([A-Za-z]+\s[A-Za-z]+)[#]");
+        }
+    }
+}
