cfdde34fbaa1e55228cb3fadfcd3658adbd11f64
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-03-26 13:20:27 +0200

    new TaskTwo/Registration

---
+++ FinalExam/TaskTwo/Registration/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Registration
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"([U]\$)([A-Z][a-z]{2,})\1([P]\@\$)([a-z]{5,}[0-9]+)\3";
+            int numLine = int.Parse(Console.ReadLine());
+            int counter = 0;
+
+            for (int i = 0; i < numLine; i++)
+            {
+                string email = Console.ReadLine();
+                var match = Regex.Match(email, pattern);
+
+                if (match.Success)
+                {
+                    counter++;
+                    Console.WriteLine("Registration was successful");
+                    Console.WriteLine($"Username: {match.Groups[2].ToString()}, Password: {match.Groups[4].ToString()}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid username or password");
+                }
+            }
+
+            Console.WriteLine($"Successful registrations: {counter}");
+        }
+    }
+}
