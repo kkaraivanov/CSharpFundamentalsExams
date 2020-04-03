2c78b89fe4a5dcbc886e916593ddc1e49adf65c4
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-04-03 14:16:07 +0300

    ferst command added

--- FinalExam/TaskTree/MessagesManager/Program.cs
+++ FinalExam/TaskTree/MessagesManager/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MessagesManager
 {
@@ -6,7 +8,30 @@ namespace MessagesManager
     {
         static void Main(string[] args)
         {
-            
+            int maxCapacity = int.Parse(Console.ReadLine());
+            var users = new Dictionary<string,Messages>();
+            while (true)
+            {
+                var commands = Console.ReadLine().Split("=").ToArray();
+                ;
+                if (commands.Contains("Statistics"))
+                {
+                    break;
+                }
+
+                if (commands.Contains("Add"))
+                {
+                    string user = commands[1];
+                    int sent = int.Parse(commands[2]);
+                    int received = int.Parse(commands[2]);
+                }
+            }
         }
+    }
+
+    class Messages
+    {
+        public int SentMessage { get; set; }
+        public int ReceivedMessage { get; set; }
     }
 }
